fb4c85c4dd8881be8ffb36389505bb99b054e2ae
unknown <coderartem@gmail.com>
2017-09-18 09:05:34 -0500

    Launcher updated

--- Data-Access-Objects/src/main/java/jdbc/lesson/Launcher.java
+++ Data-Access-Objects/src/main/java/jdbc/lesson/Launcher.java
@@ -14,30 +14,47 @@ public class Launcher {
 
 	public static void main(String[] args) {
 
-		PersonDao pD = new PersonDao();
-		LocationDao lD = new LocationDao();
-		InterestDao iD = new InterestDao();
+		PersonDao personDao = new PersonDao();
+		LocationDao locationDao = new LocationDao();
+		InterestDao interestDao = new InterestDao();
+		
+		/**
+		 * Test of get(id) methods
+		 */
+//		System.out.println(interestDao.get(new Long(20)));
+//		System.out.println(locationDao.get(new Long(20)));
+//		System.out.println(personDao.get(new Long(21)));
+		
+		/**
+		 * Test of save(object) methods with null id's (creating new entries in all DBs)
+		 */
+//		HashSet<Interest> intersts = new HashSet<>();
+//		intersts.add(new Interest(null, "Dance"));
+//		intersts.add(new Interest(null, "Run"));
+//		personDao.save(new Person(null,"Doe","Joe",new Long(25),new Location(null,"Miami","Florida","USA"), intersts));
+//		
+//		interestDao.save(new Interest("Hockey"));
 		
 		
-		//System.out.println(iD.get(new Long(20)));
-		//System.out.println(lD.get(new Long(20)));
-		//System.out.println(pD.get(new Long(21)));
+		/**
+		 * Test of save() methods  with not null id's (updating existing records in all DB's)
+		 */
+//		Set<Interest> interests = new HashSet<Interest>();
+//		interests.add(new Interest(new Long(20), "Footbal"));
+//		interests.add(new Interest("Fishing"));
+//		personDao.save(new Person(new Long(21),"Alice","Shmalice",new Long(20),new Location("WonderCity","WonderLand","WonderCountry"), interests));
+//		
+//		locationDao.save(new Location(new Long(16),"Dallas","Texas","USA"));
+//		interestDao.save(new Interest(new Long(17),"Reading"));
 		
-		/*HashSet<Interest> i = new HashSet<Interest>();
-		i.add(new Interest(null, "Dance"));
-		i.add(new Interest(null, "Run"));
-		pD.save(new Person(null,"Doe","Joe",new Long(25),new Location(null,"Miami","Florida","USA"), i));
-		*/
 		
-		//lD.save(new Location(new Long(16),"Dallas","Texas","USA"));
-		//iD.save(new Interest(new Long(17),"Reading"));
-		/*Set<Interest> k = new HashSet<Interest>();
-		k.add(new Interest(new Long(20), "Footbal"));
-		k.add(new Interest("Fishing"));
-		pD.save(new Person(new Long(21),"Alice","Shmalice",new Long(20),new Location("WonderCity","WonderLand","WonderCountry"), k));
-		*/
-		
-		//iD.save(new Interest("Hockey"));
+		/**
+		 * Test of findInterestGroup("Interest", "Location") method
+		 */
+		Set<Person> sP = personDao.findInterestGroup("fart","USA");
+		for(Person x : sP){
+			System.out.println(x);
+		}
 				
 	}
 
